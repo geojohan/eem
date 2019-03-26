9ee255246c815cb4f25339e07d015e580bfbc76e
root <root@jason.azteklab.demo>
2019-03-26 10:01:46 +0100

    Initial release

---
+++ gogit.sh
@@ -0,0 +1,7 @@
+echo "# eem" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/geojohan/eem.git
+git push -u origin master
+
---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<title>EEM</title>
+
+<?php
+echo "Hello World!";
+?>
+
+</html>
